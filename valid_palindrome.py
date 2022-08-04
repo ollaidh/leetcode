4aa52350b50647f60a18c3a85bf1209a4dfe93b9
Maria Lineva <ollaidhu@programmer.net>
2022-08-04 09:25:07 +0300

    Add valid palindrome solution

---
+++ valid_palindrome.py
@@ -0,0 +1,28 @@
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+# and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+class Solution(object):
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while l < r and not self.isAlphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+
+    def isAlphaNum(self, letter):
+        return (ord('A') <= ord(letter) <= ord('Z') or
+                ord('a') <= ord(letter) <= ord('z') or
+                ord('0') <= ord(letter) <= ord('9'))
+
+
+if __name__ == '__main__':
+    check = Solution()
+    print(check.isPalindrome('A man, a plan, a canal: Panama'))
+    print(check.isPalindrome('raceacar'))
