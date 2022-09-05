4a8604fe78eb742b72ad31c1f452dce37469f6eb
Maria Lineva <ollaidh@programmer.net>
2022-09-05 16:49:22 +0300

    Add task description

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,3 +1,9 @@
+# Given a sorted array of distinct integers and a target value,
+# return the index if the target is found.
+# If not, return the index where it would be if it were inserted in order.
+# Algorithm with O(log n) runtime complexity.
+
+
 import my_utils.testing as test
 
 
