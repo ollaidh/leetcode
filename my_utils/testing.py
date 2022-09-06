197b22ab3606e356549528ce436e7dd353239f71
Maria Lineva <ollaidh@programmer.net>
2022-09-06 15:17:47 +0300

    Add different function input processing for different data types (unpack or not unpack)

--- my_utils/testing.py
+++ my_utils/testing.py
@@ -10,6 +10,11 @@ def run_test(func, func_inp, expected, case_name=''):
     input_items = []
     for item in func_inp:
         input_items.append(str(item))
-    actual = func(*func_inp)
-    postfix = ' '.join([case_name, 'Input:', ', '.join(input_items)])
+    if type(func_inp) == 'list' or type(func_inp) == 'tuple':
+        actual = func(*func_inp)
+        postfix = ' '.join([case_name, 'Input:', ', '.join(input_items)])
+    else:
+        actual = func(func_inp)
+        postfix = ' '.join([case_name, 'Input:', '\"', func_inp, '\"'])
+    # postfix = ' '.join([case_name, 'Input:', ', '.join(input_items)])
     verify_if_equal(actual, expected, postfix)
