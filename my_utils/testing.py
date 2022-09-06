57dd24ef35384b2268dd09f04845a5a6e85db62e
Maria Lineva <ollaidh@programmer.net>
2022-09-06 13:39:40 +0300

    Change printing output, now works for linked lists (prints the list values, not the object address)

--- my_utils/testing.py
+++ my_utils/testing.py
@@ -7,6 +7,9 @@ def verify_if_equal(actual, expected, message):
 
 
 def run_test(func, func_inp, expected, case_name=''):
+    input_items = []
+    for item in func_inp:
+        input_items.append(str(item))
     actual = func(*func_inp)
-    postfix = ' '.join([case_name, 'Input:', str(func_inp)])
+    postfix = ' '.join([case_name, 'Input:', ', '.join(input_items)])
     verify_if_equal(actual, expected, postfix)
