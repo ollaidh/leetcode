03c9abfac5c5df4974d82d7ad9d4a455a421598f
Maria Lineva <ollaidh@programmer.net>
2022-09-09 13:39:57 +0300

    Add creation of two intersected linked lists (two single linked lists have one node where they intersect)

--- my_utils/linked_list.py
+++ my_utils/linked_list.py
@@ -27,6 +27,9 @@ class ListNode(object):
             curr = curr.next
         return '[' + ','.join(line) + ']'
 
+    def __hash__(self):
+        return hash((self.val, self.next))
+
 
 # creates linked list from a regular list. Takes a regular list as input, returns head of linked list
 def create_linked_list(lst):
@@ -59,6 +62,30 @@ def create_cycled_linked_list(lst):
     return head
 
 
+def create_two_intersected_lists(lst1, lst2, lst3):
+    if not lst1 or not lst2 or not lst3:
+        return None
+
+    head1, head2, head3 = ListNode(lst1[0]), ListNode(lst2[0]), ListNode(lst3[0])
+    curr1, curr2, curr3 = head1, head2, head3
+
+    for i in range(1, len(lst3)):
+        curr3.next = ListNode(lst3[i])
+        curr3 = curr3.next
+
+    for i in range(1, len(lst1)):
+        curr1.next = ListNode(lst1[i])
+        curr1 = curr1.next
+    curr1.next = head3
+
+    for i in range(1, len(lst2)):
+        curr2.next = ListNode(lst2[i])
+        curr2 = curr2.next
+    curr2.next = head3
+
+    return head1, head2, head3
+
+
 def test_eq():
     tst.verify_if_equal(create_linked_list([0, 1]) == create_linked_list([0, 1]), True, 'Simple test')
     tst.verify_if_equal(create_linked_list([0, 1]) == create_linked_list([1, 2]), False, 'Simple not eq')
@@ -68,3 +95,4 @@ def test_eq():
 
 if __name__ == '__main__':
     test_eq()
+    print(*create_two_intersected_lists([1, 2, 4], [8, 1, 6], [5, 5, 5, 5]))
