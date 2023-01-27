db7718e51cb9c4f92bdd67f821433de93e8f9cbc
Maria Lineva <ollaidh@programmer.net>
2023-01-27 20:58:05 +0200

    Add manual matrix multiply, compare of execution time for manual multiple and np.matmul

--- codewars/test_square_matrix_multiplication.py
+++ codewars/test_square_matrix_multiplication.py
@@ -4,21 +4,141 @@
 
 import unittest
 import numpy as np
+import timeit
+import argparse
+
+
+class UnableMultiplyMatricesError(Exception):
+    def __str__(self):
+        return "Matrices dimensions do not allow to multiply them. Expected: matrix1 n*m, matrix2 m*n!"
+
+
+def can_multiply(matrix1, matrix2):
+    return matrix1.shape[0] == matrix2.shape[1] and matrix1.shape[1] == matrix2.shape[0]
+
+
+def matrix_mult_np(a, b):
+    matrix1 = np.array(a)
+    matrix2 = np.array(b)
+
+    if not can_multiply(matrix1, matrix2):
+        raise UnableMultiplyMatricesError
+
+    mult = np.matmul(matrix1, matrix2)
+    return mult
+
+
+def matrix_mult_manual(a, b):
+    matrix1 = np.array(a)
+    matrix2 = np.array(b)
+
+    if not can_multiply(matrix1, matrix2):
+        raise UnableMultiplyMatricesError
+
+    mult = np.full(
+        shape=(matrix1.shape[0], matrix2.shape[1]),
+        fill_value=0,
+        dtype=int
+    )
+
+    for i in range(matrix1.shape[0]):
+        for j in range(matrix2.shape[1]):
+            for k in range(matrix1.shape[1]):
+                mult[i, j] += matrix1[i, k] * matrix2[k, j]
+
+    return mult
 
 
 def matrix_mult(a, b):
-    matrix1 = np.array(a)
-    matrix2 = np.array(b)
-    matrix_result = np.matmul(matrix1, matrix2)
-    return [list(line) for line in matrix_result]
+    return [list(i) for i in matrix_mult_np(a, b)]
 
 
-class TestMatrMult(unittest.TestCase):
+def runtime_compare():
+    time_manual = timeit.timeit(
+        lambda: matrix_mult_manual([[1, 2, 3], [3, 2, 1], [2, 1, 3]], [[4, 5, 6], [6, 5, 4], [4, 6, 5]]),
+        number=100000
+    )
+
+    time_np = timeit.timeit(
+        lambda: np.matmul([[1, 2, 3], [3, 2, 1], [2, 1, 3]], [[4, 5, 6], [6, 5, 4], [4, 6, 5]]),
+        number=100000
+    )
+
+    print('Time manual:', time_manual, 'Time using np.matmul:', time_np)
+
+
+class TestMatricesMult(unittest.TestCase):
+    def test_can_multiply(self):
+        self.assertTrue(
+            can_multiply(
+                np.array([[1, 2, 3], [3, 2, 1], [2, 1, 3]]),
+                np.array([[4, 5, 6], [6, 5, 4], [4, 6, 5]])
+            )
+        )
+        self.assertTrue(
+            can_multiply(
+                np.array([[1, 2, 3], [3, 2, 1]]),
+                np.array([[4, 5], [6, 5], [4, 6]])
+            )
+        )
+        self.assertFalse(
+            can_multiply(
+                np.array([[1, 2, 3], [3, 2, 1], [2, 1, 3]]),
+                np.array([[6, 5, 4], [4, 6, 5]])
+            )
+        )
+
+    def test_matrix_mult_manual(self):
+        arr1 = np.array([[1, 2], [3, 2]])
+        arr2 = np.array([[3, 2], [1, 1]])
+        self.assertTrue(
+            (matrix_mult_manual(arr1, arr2) == np.matmul(arr1, arr2)).all()
+        )
+
+        arr3 = np.array([[1, 2, 3], [3, 2, 1], [2, 1, 3]])
+        arr4 = np.array([[4, 5, 6], [6, 5, 4], [4, 6, 5]])
+        self.assertTrue(
+            (matrix_mult_manual(arr3, arr4) == np.matmul(arr3, arr4)).all()
+        )
+
+        arr3 = np.array([[1, 2], [3, 2], [2, 1]])
+        arr4 = np.array([[4, 5, 6], [6, 5, 4]])
+        self.assertTrue(
+            (matrix_mult_manual(arr3, arr4) == np.matmul(arr3, arr4)).all()
+        )
+
+        self.assertRaises(UnableMultiplyMatricesError, matrix_mult_np, [[1, 2, 3], [3, 2, 1], [2, 1, 3]],
+                          [[6, 5, 4], [4, 6, 5]])
+
     def test_matrix_mult(self):
-        self.assertEqual(matrix_mult([[1, 2], [3, 2]], [[3, 2], [1, 1]]), [[5, 4], [11, 8]])
-        self.assertEqual(matrix_mult([[1, 2, 3], [3, 2, 1], [2, 1, 3]], [[4, 5, 6], [6, 5, 4], [4, 6, 5]]),
-                         [[28, 33, 29], [28, 31, 31], [26, 33, 31]])
+        arr1 = np.array([[1, 2], [3, 2]])
+        arr2 = np.array([[3, 2], [1, 1]])
+        self.assertTrue(
+            (matrix_mult_np(arr1, arr2) == np.matmul(arr1, arr2)).all()
+        )
+
+        arr3 = np.array([[1, 2, 3], [3, 2, 1], [2, 1, 3]])
+        arr4 = np.array([[4, 5, 6], [6, 5, 4], [4, 6666666, 5]])
+        self.assertTrue(
+            (matrix_mult_np(arr3, arr4) == np.matmul(arr3, arr4)).all()
+        )
+
+        arr3 = np.array([[1, 2], [3, 2], [2, 1]])
+        arr4 = np.array([[4, 5, 6], [6, 5, 4]])
+        self.assertTrue(
+            (matrix_mult_np(arr3, arr4) == np.matmul(arr3, arr4)).all()
+        )
+
+        self.assertRaises(UnableMultiplyMatricesError, matrix_mult_np, [[1, 2, 3], [3, 2, 1], [2, 1, 3]],
+                          [[6, 5, 4], [4, 6, 5]])
 
 
 if __name__ == '__main__':
-    unittest.main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', '--benchmark', action='store_true')
+    args = parser.parse_args()
+
+    if args.benchmark:
+        runtime_compare()
+    else:
+        unittest.main()
