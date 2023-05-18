284f1855edeef44cb0563875fd727e395c5dd236
ollaidh <ollaidh@programmer.net>
2023-05-18 15:36:03 +0300

    Exercise from "Python Workout" by Reuven Lerner: sort list of tuples by multiple parameters (using @classmethod)

--- python_workout_lerner/sort_list_tuples.py
+++ python_workout_lerner/sort_list_tuples.py
@@ -2,27 +2,30 @@ import unittest
 import operator
 
 
-def sort_films(films: list[tuple], *sorting_parameters: str) -> list[tuple]:
+class Films:
     sortings = {'name': 0, 'director': 1, 'length': 2}
-    items = [sortings[param] for param in sorting_parameters]
-    films_sorted = [film for film in sorted(films, key=operator.itemgetter(*items))]
 
-    return films_sorted
+    @classmethod
+    def sort_films(cls, films, *sorting_parameters: str):
+        items = [cls.sortings[param] for param in sorting_parameters]
+        films_sorted = [film for film in sorted(films, key=operator.itemgetter(*items))]
+        return films_sorted
 
 
 class TestSort(unittest.TestCase):
     def test_sort_films(self):
         self.assertEqual(
             [('Film1', 'Director9', 5), ('Film4', 'Director2', 1), ('Film4', 'Director5', 2)],
-            sort_films([('Film4', 'Director5', 2), ('Film4', 'Director2', 1), ('Film1', 'Director9', 5)],
-                       'name', 'director'))
+            Films.sort_films([('Film4', 'Director5', 2), ('Film4', 'Director2', 1), ('Film1', 'Director9', 5)],
+                             'name', 'director'))
         self.assertEqual(
             [('Film2', 'Director2', 1), ('Film4', 'Director5', 2), ('Film1', 'Director9', 5)],
-            sort_films([('Film4', 'Director5', 2), ('Film2', 'Director2', 1), ('Film1', 'Director9', 5)], 'director'))
+            Films.sort_films([('Film4', 'Director5', 2), ('Film2', 'Director2', 1), ('Film1', 'Director9', 5)],
+                             'director'))
         self.assertEqual(
             [('Film2', 'Director2', 1), ('Film2', 'Director2', 2), ('Film2', 'Director2', 5)],
-            sort_films([('Film2', 'Director2', 2), ('Film2', 'Director2', 1), ('Film2', 'Director2', 5)],
-                       'name', 'director', 'length'))
+            Films.sort_films([('Film2', 'Director2', 2), ('Film2', 'Director2', 1), ('Film2', 'Director2', 5)],
+                             'name', 'director', 'length'))
 
 
 if __name__ == '__main__':
