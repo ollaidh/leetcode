145b02d644bc09e929a7dc35ed17a2aa90c09409
ollaidh <ollaidh@programmer.net>
2023-05-29 17:23:42 +0300

    Exercise from "Python Workout" by Reuven Lerner: 20. Changed getting file size method

--- python_workout_lerner/test_20_file_reports.py
+++ python_workout_lerner/test_20_file_reports.py
@@ -1,12 +1,12 @@
 import glob
+import os
 import unittest
 import pathlib
-from os.path import isfile, getsize
 
 
 def get_files_size(filepath: pathlib.Path) -> dict:
     files = glob.glob(str(filepath) + '/*')
-    files_info = {pathlib.Path(f).name: getsize(f) for f in files if isfile(f)}
+    files_info = {pathlib.Path(f).name: os.stat(f).st_size for f in files if os.path.isfile(f)}
 
     return files_info
 
