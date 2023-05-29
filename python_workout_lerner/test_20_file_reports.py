3b77ea82e44d065d3dc328e43b9e4b958e0ecdea
ollaidh <ollaidh@programmer.net>
2023-05-29 18:31:55 +0300

    Exercise from "Python Workout" by Reuven Lerner: 20. File size in kb

--- python_workout_lerner/test_20_file_reports.py
+++ python_workout_lerner/test_20_file_reports.py
@@ -4,9 +4,15 @@ import unittest
 import pathlib
 
 
+def get_size_kb(filepath: str) -> float:
+    size_b = os.stat(filepath).st_size
+    return round(size_b/1024, 1)
+
+
 def get_files_size(filepath: pathlib.Path) -> dict:
     files = glob.glob(str(filepath) + '/*')
-    files_info = {pathlib.Path(f).name: os.stat(f).st_size for f in files if os.path.isfile(f)}
+    files_info = {pathlib.Path(f).name: get_size_kb(f) for f in files if os.path.isfile(f)}
+    print(files_info)
 
     return files_info
 
@@ -15,7 +21,7 @@ class TestFilesReports(unittest.TestCase):
     def test_get_files_size(self):
         file_path = pathlib.Path(__file__).parent.resolve() / 'files_size_test'
         self.assertEqual(
-            {'wcfile.txt': 165, 'sentences.txt': 211, 'passwds.txt': 5631},
+            {'wcfile.txt': 0.2, 'sentences.txt': 0.2, 'passwds.txt': 5.5},
             get_files_size(file_path)
         )
 
