3b118de1117af29350dc37524a9ab1dd4f048cd7
ollaidh <ollaidh@programmer.net>
2023-06-08 16:34:27 +0300

    Exercise from "Python Workout" by Reuven Lerner: 22. Tests with temporary directory, optimised strtswith '#' check

--- python_workout_lerner/test_22_read_write_csv.py
+++ python_workout_lerner/test_22_read_write_csv.py
@@ -1,10 +1,11 @@
 import pathlib
 import csv
 import os
+import tempfile
 import unittest
 
 
-def passwd_to_csv(input_file: pathlib.Path, output_file: pathlib.Path) -> None:
+def passwd_to_csv(input_file: pathlib.Path, output_file) -> None:
 
     with open(input_file, 'r') as scvinp, open(output_file, 'w') as csvout:
         inp_reader = csv.reader(scvinp, delimiter=':')
@@ -12,24 +13,25 @@ def passwd_to_csv(input_file: pathlib.Path, output_file: pathlib.Path) -> None:
         for row in inp_reader:
             if row[0].startswith('#'):
                 continue
-            inp_writer.writerow([''.join(row[0])] + [''.join(row[2])])
+            inp_writer.writerow((row[0], row[2]))
 
 
 class TestCsv(unittest.TestCase):
     def test_passwd_to_csv(self):
-        filepath_inp = pathlib.Path(__file__).parent.resolve() / '22_read_write_csv' / 'input.csv'
-        filepath_out = pathlib.Path(__file__).parent.resolve() / '22_read_write_csv' / 'output.csv'
-        passwd_to_csv(filepath_inp, filepath_out)
-        with open(filepath_out) as f:
-            file_lines = f.readlines()
-            self.assertEqual(3, len(file_lines))
-            self.assertEqual(2, len(file_lines[0].split('\t')))
-            self.assertEqual('root', file_lines[0].split('\t')[0])
-            self.assertEqual('0', file_lines[0].split('\t')[1].rstrip())
-            self.assertEqual(2, len(file_lines[2].split('\t')))
-            self.assertEqual('_ftp', file_lines[2].split('\t')[0])
-            self.assertEqual('98', file_lines[2].split('\t')[1].rstrip())
-        os.remove(filepath_out)
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            filepath_inp = pathlib.Path(__file__).parent.resolve() / '22_read_write_csv' / 'input.csv'
+            filepath_out = pathlib.Path(tmpdirname) / 'output.csv'
+            passwd_to_csv(filepath_inp, filepath_out)
+            f = open(filepath_out)
+            with open(filepath_out) as f:
+                file_lines = f.readlines()
+                self.assertEqual(3, len(file_lines))
+                self.assertEqual(2, len(file_lines[0].split('\t')))
+                self.assertEqual('root', file_lines[0].split('\t')[0])
+                self.assertEqual('0', file_lines[0].split('\t')[1].rstrip())
+                self.assertEqual(2, len(file_lines[2].split('\t')))
+                self.assertEqual('_ftp', file_lines[2].split('\t')[0])
+                self.assertEqual('98', file_lines[2].split('\t')[1].rstrip())
 
 
 if __name__ == '__main__':
