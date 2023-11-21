48c5a57331355259f9d09f8f143922862db1e1f4
ollaidh <ollaidh@programmer.net>
2023-11-21 13:31:58 +0200

    LeetCode No. 226. Invert Binary Tree - add iterative inversion function

--- leetcode/test_226_invert_binary_tree.py
+++ leetcode/test_226_invert_binary_tree.py
@@ -9,11 +9,25 @@ from my_utils.trees import *
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # recursive
         if not root:
             return None
         root.left, root.right = root.right, root.left
         self.invertTree(root.left)
         self.invertTree(root.right)
+        return root
+
+    def invertTreeIter(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # iterative
+        cells_analyze = [root]
+        while cells_analyze:
+            curr = cells_analyze.pop()
+            if curr:
+                curr.left, curr.right = curr.right, curr.left
+                if curr.left:
+                    cells_analyze.append(curr.left)
+                if curr.right:
+                    cells_analyze.append(curr.right)
         return root
 
 
@@ -32,3 +46,18 @@ class TestSolution(unittest.TestCase):
         input3 = create_binary_tree([])
         expected3 = create_binary_tree([])
         self.assertEqual(expected3, solution.invertTree(input3))
+
+    def test_invertTreeIter(self):
+        solution = Solution()
+
+        input1 = create_binary_tree([4, 2, 7, 1, 3, 6, 9])
+        expected1 = create_binary_tree([4, 7, 2, 9, 6, 3, 1])
+        self.assertEqual(expected1, solution.invertTreeIter(input1))
+
+        input2 = create_binary_tree([2, 1, 3])
+        expected2 = create_binary_tree([2, 3, 1])
+        self.assertEqual(expected2, solution.invertTreeIter(input2))
+
+        input3 = create_binary_tree([])
+        expected3 = create_binary_tree([])
+        self.assertEqual(expected3, solution.invertTreeIter(input3))
