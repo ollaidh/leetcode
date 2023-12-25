654d605607cb23d49e8067d577731f21fedc0709
ollaidh <ollaidh@programmer.net>
2023-12-25 15:24:35 +0200

    Advent Of Code: Day 12. Hot Springs. Part 1.

--- adventofcode_2023/test_day_12.py
+++ adventofcode_2023/test_day_12.py
@@ -1,97 +1,108 @@
 import unittest
 from dataclasses import dataclass
+import pathlib
 
 
 @dataclass
 class SpringPack:
-    springs: list[str]
+    springs: str
     pattern: list[int]
 
 
 def get_line_parameters(line: str) -> SpringPack:
-    springs = list(line.split()[0])
+    springs = line.split()[0]
     pattern = [int(letter) for letter in line.split()[1].split(',')]
     return SpringPack(springs, pattern)
 
 
-def startswith_damaged(pack: list[str], number: int) -> bool:
-    if not pack:
-        return False
-    for i in range(0, number - 1):
-        if pack[i] != '#':
-            return False
-    if len(pack) > number and pack[number] == '#':
-        return False
-    return True
-
-
-def check_pattern_match(pack: SpringPack):
-    start = 0
-    while pack.springs and pack.springs[0] != '#':
-        pack.springs = pack.springs[1:]
-    if not pack.springs and not pack.pattern:
-        return True
-    if not startswith_damaged(pack.springs[start:], pack.pattern[0]):
-        return False
-    pack.springs = pack.springs[pack.pattern[0]:]
-    pack.pattern = pack.pattern[1:]
-    return check_pattern_match(SpringPack(pack.springs, pack.pattern))
+def replace_element(line: str, position: int) -> tuple[str, str]:
+    if position >= len(line):
+        stop = 0
+    line_damaged = [c for c in line]
+    line_operational = [c for c in line]
+    line_damaged[position] = '#'
+    line_operational[position] = '.'
+    return ''.join(line_operational), ''.join(line_damaged)
 
 
 def count_arrangements(pack: SpringPack) -> int:
-    result = 0
+    while pack.springs.startswith('.'):
+        pack.springs = pack.springs[1:]
+
     if not pack.springs and not pack.pattern:
         return 1
-    if pack.springs and not pack.pattern:
+    if not pack.springs and pack.pattern:
         return 0
+
+    damaged_prefix_len = 0
+    if pack.springs.startswith('#'):
+        if not pack.pattern:
+            return 0
+        while damaged_prefix_len < len(pack.springs) and pack.springs[damaged_prefix_len] == "#":
+            damaged_prefix_len += 1
+
+        if damaged_prefix_len > pack.pattern[0]:
+            return 0
+        if damaged_prefix_len == len(pack.springs):
+            if damaged_prefix_len != pack.pattern[0] or len(pack.pattern) > 1:
+                return 0
+            return 1
+
+        #  '#' followed by '.':
+        if pack.springs[damaged_prefix_len] == '.':
+            if damaged_prefix_len != pack.pattern[0]:
+                return 0
+            pack.springs = pack.springs[damaged_prefix_len:]
+            pack.pattern = pack.pattern[1:]
+            return count_arrangements(pack)
+
+    #  '#' followed by '?', or starts with '?':
+    operational, damaged = replace_element(pack.springs, damaged_prefix_len)
+    return count_arrangements(SpringPack(operational, pack.pattern)) + count_arrangements(
+        SpringPack(damaged, pack.pattern))
+
+
+def play(input_path: str) -> int:
+    filepath_inp = pathlib.Path(__file__).parent.resolve() / input_path
+    result = 0
+    with open(filepath_inp) as f:
+        for line in f:
+            result += count_arrangements(get_line_parameters(line))
+
+    return result
 
 
 class TestHotSprings(unittest.TestCase):
     def test_get_line_parameters(self):
-        expected = SpringPack(['?', '?', '?', '.', '#', '#', '#'],
+        expected = SpringPack('???.###',
                               [1, 1, 3])
         self.assertEqual(expected, get_line_parameters("???.### 1,1,3"))
 
-        expected = SpringPack(['?', '#', '?', '#', '?', '#', '?', '#', '?', '#', '?', '#', '?', '#', '?'],
+        expected = SpringPack('?#?#?#?#?#?#?#?',
                               [1, 3, 1, 6])
         self.assertEqual(expected, get_line_parameters("?#?#?#?#?#?#?#? 1,3,1,6"))
 
-    def test_startswith_damaged(self):
-        pack1 = ['#', '#', '#', '.', '.']
-        self.assertTrue(startswith_damaged(pack1, 3))
-        self.assertFalse(startswith_damaged(pack1, 2))
-
-        pack2 = ['.', '#', '#', '.', '.']
-        self.assertFalse(startswith_damaged(pack2, 2))
-
-    def test_check_pattern_match(self):
-        pack1 = SpringPack(
-            ['#', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#', '.'],
-            [1, 6, 5])
-        self.assertTrue(check_pattern_match(pack1))
-
-        pack2 = SpringPack(
-            ['.', '#', '.', '#', '#', '#', '.', '#', '.', '#', '#', '#', '#', '#', '#'],
-            [1, 3, 1, 6])
-        self.assertTrue(check_pattern_match(pack2))
-
-        pack3 = SpringPack(
-            ['#', '.', '#', '.', '#', '#', '#'],
-            [1, 1, 3])
-        self.assertTrue(check_pattern_match(pack3))
-
     def test_count_arrangements(self):
+        pack = SpringPack(
+            '?',
+            [1])
+        self.assertEqual(1, count_arrangements(pack))
+
         pack1 = SpringPack(
-            ['?', '?', '?', '?', '.', '#', '#', '#', '#', '#', '#', '.', '.', '#', '#', '#', '#', '#', '.'],
+            '????.######..#####.',
             [1, 6, 5])
         self.assertEqual(4, count_arrangements(pack1))
 
         pack2 = SpringPack(
-            ['?', '#', '?', '#', '?', '#', '?', '#', '?', '#', '?', '#', '?', '#', '?'],
+            '?#?#?#?#?#?#?#?',
             [1, 3, 1, 6])
         self.assertEqual(1, count_arrangements(pack2))
 
         pack3 = SpringPack(
-            ['?', '#', '#', '#', '?', '?', '?', '?', '?', '?', '?'],
+            '?###????????',
             [3, 2, 1])
         self.assertEqual(10, count_arrangements(pack3))
+
+
+if __name__ == '__main__':
+    print(play('inputs/test_day_12.dat'))
