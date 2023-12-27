6f8d65d8f69ee74296088aa3a4b8052d9616af14
ollaidh <ollaidh@programmer.net>
2023-12-27 04:26:56 +0200

    Advent Of Code: Day 13. Point of Incidence. Part 2.

--- adventofcode_2023/test_day_13.py
+++ adventofcode_2023/test_day_13.py
@@ -31,7 +31,46 @@ def find_symetry(matrix: list[list[str]]) -> tuple[int, str]:
     assert False
 
 
-def play(input_path: str) -> int:
+def repair_smudge(row1: list[str], row2: list[str]) -> bool:
+    diff_indexes = []
+    for i in range(len(row1)):
+        if row1[i] != row2[i]:
+            diff_indexes.append(i)
+        if len(diff_indexes) > 1:
+            return False
+    return True
+
+
+def find_smudged_symetry(matrix: list[list[str]]) -> tuple[int, str]:
+    symmetry_types = {'hor': matrix, 'vert': rotate_matrix(matrix)}
+    for symmetry_type in symmetry_types:
+        field = symmetry_types[symmetry_type]
+        for i in range(len(field) - 1):
+            repaired = False
+            symmetry = True
+            if field[i] == field[i + 1] or repair_smudge(field[i], field[i + 1]) is True:
+                if field[i] != field[i + 1]:
+                    repaired = True
+                    if i == 0:
+                        return 1, symmetry_type
+                up_index = i - 1
+                down_index = i + 2
+                while up_index >= 0 and down_index <= len(field) - 1:
+                    if field[up_index] != field[down_index]:
+                        if repaired is True or repair_smudge(field[up_index], field[down_index]) is False:
+                            symmetry = False
+                            break
+                        else:
+                            repaired = True
+                    up_index -= 1
+                    down_index += 1
+                if symmetry and repaired is True:
+                    return i + 1, symmetry_type
+    assert False
+
+
+def play(input_path: str, symmetry_type: str) -> int:
+    symmetry_types = {'straight': find_symetry, 'smudged': find_smudged_symetry}
     filepath_inp = pathlib.Path(__file__).parent.resolve() / input_path
     result = 0
     with open(filepath_inp) as f:
@@ -43,7 +82,7 @@ def play(input_path: str) -> int:
             if line:
                 block.append([symbol for symbol in line])
             if line_id == last_line_id or not line:
-                curr_result, type_sym = find_symetry(block)
+                curr_result, type_sym = symmetry_types[symmetry_type](block)
                 if type_sym == 'hor':
                     result += 100 * curr_result
                 else:
@@ -108,10 +147,58 @@ class TestIncidence(unittest.TestCase):
         ]
         self.assertEqual((5, 'vert'), find_symetry(field))
 
+    def test_find_smudged_symetry(self):
+        field = [
+            ['#', '.', '.', '.', '#', '#', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '#', '.', '.', '#'],
+            ['.', '.', '#', '#', '.', '.', '#', '#', '#'],
+            ['#', '#', '#', '#', '#', '.', '#', '#', '.'],
+            ['#', '#', '#', '#', '#', '.', '#', '#', '.'],
+            ['.', '.', '#', '#', '.', '.', '#', '#', '#'],
+            ['#', '.', '.', '.', '.', '#', '.', '.', '#']
+        ]
+        self.assertEqual((1, 'hor'), find_smudged_symetry(field))
+
+        field = [
+            ['#', '.', '#', '#', '.', '.', '#', '#', '.'],
+            ['.', '.', '#', '.', '#', '#', '.', '#', '.'],
+            ['#', '#', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '#', '.', '.', '.', '.', '.', '.', '#'],
+            ['.', '.', '#', '.', '#', '#', '.', '#', '.'],
+            ['.', '.', '#', '#', '.', '.', '#', '#', '.'],
+            ['#', '.', '#', '.', '#', '#', '.', '#', '.']
+        ]
+        self.assertEqual((3, 'hor'), find_smudged_symetry(field))
+
+        field = [
+            ['#', '.', '#', '#', '.', '#', '#'],
+            ['#', '#', '#', '#', '#', '#', '.'],
+            ['.', '#', '.', '.', '#', '.', '.'],
+            ['#', '.', '#', '#', '.', '#', '#'],
+            ['.', '#', '.', '.', '#', '.', '#'],
+            ['.', '.', '.', '.', '.', '.', '#'],
+            ['.', '#', '.', '.', '#', '.', '#'],
+            ['.', '#', '.', '.', '#', '.', '#'],
+            ['#', '.', '.', '.', '.', '#', '.'],
+            ['#', '.', '.', '.', '.', '#', '#'],
+            ['#', '.', '.', '.', '.', '#', '#'],
+            ['#', '.', '.', '.', '.', '#', '.'],
+            ['.', '#', '.', '.', '#', '.', '#'],
+            ['.', '#', '.', '.', '#', '.', '#'],
+            ['.', '.', '.', '.', '.', '.', '#'],
+            ['.', '#', '.', '.', '#', '.', '#'],
+            ['#', '.', '#', '#', '.', '#', '.']
+        ]
+        self.assertEqual((10, 'hor'), find_smudged_symetry(field))
+
     def test_play(self):
-        result = play('inputs_tests/input_day_13_test.dat')
+        result = play('inputs_tests/input_day_13_test.dat', 'straight')
         self.assertEqual(405, result)
+
+        result = play('inputs_tests/input_day_13_test.dat', 'smudged')
+        self.assertEqual(400, result)
 
 
 if __name__ == '__main__':
-    print(play('inputs/input_day_13.dat'))
+    print(play('inputs/input_day_13.dat', 'straight'))
+    print(play('inputs/input_day_13.dat', 'smudged'))
