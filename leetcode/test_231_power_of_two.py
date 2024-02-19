44c9d9089ea6fdf4cbc44261fb7535689dbc6f56
ollaidh <ollaidh@programmer.net>
2024-02-19 12:22:51 +0200

    LeetCode No. 231. Power of Two

---
+++ leetcode/test_231_power_of_two.py
@@ -0,0 +1,21 @@
+# Given an integer n, return true if it is a power of two.
+# Otherwise, return false.
+
+
+import unittest
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n.bit_count() == 1 and bin(n)[2] == "1"
+
+
+class TestSolution(unittest.TestCase):
+    def test_isPowerOfTwo(self):
+        solution = Solution()
+        self.assertTrue(solution.isPowerOfTwo(8))
+        self.assertTrue(solution.isPowerOfTwo(256))
+        self.assertTrue(solution.isPowerOfTwo(1024))
+        self.assertFalse(solution.isPowerOfTwo(22))
+        self.assertFalse(solution.isPowerOfTwo(0))
+        self.assertFalse(solution.isPowerOfTwo(1000))
